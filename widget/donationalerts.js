d18f5ce91cf5936887380c181dd59a95967b9ffc
declider <62831308+declider@users.noreply.github.com>
2023-01-11 22:54:57 +0300

    Update donationalerts.js

--- widget/donationalerts.js
+++ widget/donationalerts.js
@@ -25,15 +25,16 @@ async function startDA() {
     })
 
     centrifugeDA.on('connect', (e) => {
+        centrifugeDA.subscribe('$alerts:donation_'+daid, message => {
+            console.log("Подписан на DonationAlerts")
+            let sum = message.data.amount_in_user_currency
+            console.log(message)
+            add_sum(sum)
+        })
         console.log("Подключен DonationAlerts")
     })
     
-    centrifugeDA.subscribe('$alerts:donation_'+daid, message => {
-        let sum = message.data.amount_in_user_currency
-        console.log(message)
-        add_sum(sum)
-    })
-
+   
     if(!centrifugeDA.isConnected()){
         centrifugeDA.connect()
     }

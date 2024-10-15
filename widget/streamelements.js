6511bdac2e718b30bbfef21a386278436423f5ae
declider <62831308+declider@users.noreply.github.com>
2024-10-15 19:18:13 +0300

    Update streamelements.js

--- widget/streamelements.js
+++ widget/streamelements.js
@@ -1,25 +1,30 @@
 const jwt = params.get("seToken")
 
-const se = io('https://realtime.streamelements.com', {
-	transports: ['websocket']
-})
-
-se.on('connect', onConnect)
-
-se.on('event', (data) => {
-	if (data.type != "tip") { return }
-	if (data.data.currency != "RUB") { return }
-	donate(data.data.amount)
-})
-
-se.on('event:test', (data) => {
-	if (data.type != "tip") { return }
-	if (data.data.currency != "RUB") { return }
-	donate(data.data.amount)
-})
-
-function onConnect() {
-	se.emit('authenticate', {method: 'jwt', token: jwt})
-	console.log("Подключен Streamelements")
+function connectSE() {
+	const se = io('https://realtime.streamelements.com', {
+		transports: ['websocket']
+	})
+	
+	se.on('connect', onConnect)
+	
+	se.on('event', (data) => {
+		if (data.type != "tip") { return }
+		if (data.data.currency != "RUB") { return }
+		donate(data.data.amount)
+	})
+	
+	se.on('event:test', (data) => {
+		if (data.type != "tip") { return }
+		if (data.data.currency != "RUB") { return }
+		donate(data.data.amount)
+	})
+	
+	function onConnect() {
+		se.emit('authenticate', {method: 'jwt', token: jwt})
+		console.log("Подключен Streamelements")
+	}
 }
 
+if (jwt) {
+	connectSE()
+}
